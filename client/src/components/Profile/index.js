ae59a088121a685343c873fcd28a7bf93ef9daf8
idevwork <bizwork1114@outlook.com>
2019-07-05 15:36:22 -0500

    - finished header

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
+import './style.scss';
+import Profile from '../../assets/images/profile.png';
 export default () => (
-  <div>Profile</div>
+  <div id="profile">
+    <img src={Profile} alt="image"/>
+    <div className="desc">
+      <div className="name">Stephen Austin</div>
+      <div className="occupation">Crypto Trader</div>
+    </div>
+    <svg width="9" height="5" viewBox="0 0 9 5" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M4.5 5L9 0H0L4.5 5Z" fill="#A7A9C0"/>
+    </svg>
+
+  </div>
 );
