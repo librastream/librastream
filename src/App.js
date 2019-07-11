dd78be12ec92383addf82f25bcf7004d237596d9
idevwork <bizwork1114@outlook.com>
2019-07-11 05:22:16 -0500

    - issue #3: "Implement latest designs" * finished footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Overview from './components/Overview/overview';
 import Header from './components/Header/header';
+import Footer from './components/Footer/footer';
 import AddressOverview from './components/AddressOverview/address-overview';
 import TransactionDetails from './components/TransactionDetails';
 
@@ -13,6 +14,7 @@ class App extends Component {
         <Route exact path="/" component={Overview}/>
         <Route exact path="/address-overview" component={AddressOverview}/>
         <Route exact path="/transaction-details" component={TransactionDetails}/>
+        <Footer/>
       </div>
     );
   }
