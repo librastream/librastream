3f4c35494db01b643bd0af9bad6bd7883dd91314
idevwork <bizwork1114@outlook.com>
2019-07-11 04:00:00 -0500

    - issue #3: "Implement latest designs" * finished address-overview

---
+++ src/components/QRCode/qrcode.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './qrcode.scss';
+
+export default () => (
+  <div id="qrcode" className="h-100">
+    <div className="qrcode h-100 w-100">
+    </div>
+  </div>
+);
