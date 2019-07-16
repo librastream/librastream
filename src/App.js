169e61d66a51044647035d41a93b590ac429c30b
Wisdom Oparaocha <wisop112@hotmail.com>
2019-07-16 23:23:11 +0100

    add analytics

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
-import TransactionDetails from 'components/TransactionDetails/transaction-details';
-import AddressOverview from 'components/AddressOverview/address-overview';
-import DiscoverMore from 'components/DiscoverMore/discover-more';
-import Overview from 'components/Overview/overview';
-import Header from 'components/Header/header';
-import Footer from 'components/Footer/footer';
-import 'styles/index.scss';
+import TransactionDetails from './components/TransactionDetails/transaction-details';
+import AddressOverview from './components/AddressOverview/address-overview';
+import DiscoverMore from './components/DiscoverMore/discover-more';
+import Overview from './components/Overview/overview';
+import Header from './components/Header/header';
+import Footer from './components/Footer/footer';
+import './styles/index.scss';
 
 class App extends Component {
   render() {
